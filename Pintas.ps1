bbf03d887a3301ca784c3a97bfbaf50ee1974353
Narendra Akmal <317161292+narendra-akmal@users.noreply.github.com>
2026-08-28 11:03:28 +0700

    Update Pintas.ps1

--- Pintas.ps1
+++ Pintas.ps1
@@ -133,7 +133,7 @@ Write-Host ""
 $daftarAplikasi = @(
     @{ Nama = "Opera Browser";         ID = "XP8CF6S8G2D5T6" },
     @{ Nama = "ONLYOFFICE";            ID = "ONLYOFFICE.DesktopEditors" },
-    @{ Nama = "GIMP";               ID = "GIMP.GIMP" },
+    @{ Nama = "Pixlr E";               ID = "9NWJ8JHT6WGW" },
     @{ Nama = "VLC Media Player";        ID = "VideoLAN.VLC" },
     @{ Nama = "WinRAR";                  ID = "RARLab.WinRAR" },
     @{ Nama = "Foxit Reader";             ID = "Foxit.FoxitReader" },
